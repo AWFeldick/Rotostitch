f32bab377a2be8854288b78e4b020791e85c6e9c
AWFeldick <awfeldick@gmail.com>
2015-02-25 21:38:19 -0600

    Tweaked binary building setup

--- rotostitch/__init__.py
+++ rotostitch/__init__.py
@@ -3,5 +3,7 @@ import sys
 
 __version__ = "1.0.0"
 
-packageDir = os.path.dirname(sys.argv[0])
+packageDir = os.path.dirname(__file__)
 RESOURCE_DIR = os.path.join(os.path.abspath(packageDir), "resources")
+if not os.path.isdir(RESOURCE_DIR):
+    RESOURCE_DIR = os.path.join(os.path.dirname(sys.argv[0]), "resources")
